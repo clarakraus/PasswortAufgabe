b70a9dc366b2360390e387bc28aacd486410245d
Clara Kraus <claramariakrausgmail.com>
2022-05-20 16:43:25 +0200

    backup2

--- src/main/java/PasswortChecker.java
+++ src/main/java/PasswortChecker.java
@@ -2,21 +2,44 @@ public class PasswortChecker {
 
 
     public static boolean pwLength(String password) {
-        if (password.length() >= 8){
+        if (password.length() >= 8) {
             return true;
-        }
-        else {
+        } else {
             return false;
         }
     }
 
 
     public static boolean pwNumber(String password) {
-        if(password.matches("(.*[0-9].*)")){
+        if (password.matches("(.*[0-9].*)")) {
             return true;
-        }
-        else {
+        } else {
             return false;
         }
     }
+
+    public static boolean pwLetter(String password) {
+        if (password.matches("(.*[a-z].*)") && password.matches("(.*[A-Z].*)")) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public static String checkPassword(String password) {
+        if (pwLength(password) == true) {
+            if (pwNumber(password) == true) {
+                if (pwLetter(password) == true) {
+                    return "Your password is valid";
+                } else {
+                    return "Your password should contain lower AND uppercase letters";
+                }
+            } else {
+                return "Your password should contain numbers";
+            }
+        } else {
+            return "Your Password should be at least 8 characters long";
+        }
+    }
 }
+
--- src/test/java/TestClassTest.java
+++ src/test/java/TestClassTest.java
@@ -5,7 +5,6 @@ import static org.junit.jupiter.api.Assertions.*;
 class TestClassTest {
 
     @Test
-
     void shouldBeTrueIfStringLongerThan8Char() {
         //given
         String password = "12345678";
@@ -18,8 +17,8 @@ class TestClassTest {
         assertEquals(actual, expected);
 
     }
-    @Test
 
+    @Test
     void shouldBeFalseIfShorterThan8Char() {
         //given
         String password = "1234567";
@@ -32,12 +31,12 @@ class TestClassTest {
         assertEquals(actual, expected);
 
     }
-    @Test
 
-    void shouldBeTrueIfPasswordContainsNumbers(){
+    @Test
+    void shouldBeTrueIfPasswordContainsNumbers() {
         // given
         String password = "Hallo123";
-        boolean expected =  true;
+        boolean expected = true;
         //when
         boolean actual = PasswortChecker.pwNumber(password);
         //then
@@ -45,11 +44,10 @@ class TestClassTest {
     }
 
     @Test
-
-    void shouldBeFalseIfPasswordDoesNotContainsNumbers(){
+    void shouldBeFalseIfPasswordDoesNotContainsNumbers() {
         // given
         String password = "Halloserdftzguhijok";
-        boolean expected =  false;
+        boolean expected = false;
         //when
         boolean actual = PasswortChecker.pwNumber(password);
         //then
@@ -57,15 +55,51 @@ class TestClassTest {
     }
 
     @Test
-
-    void shouldBeTrueIfPasswordContainsSmallLetters(){
+    void shouldBeTrueIfPasswordContainsSmallLetters() {
         // given
         String password = "sdfghjLKjH";
         boolean expected = true;
         //when
-        boolean actual = PasswortChecker.pwSmallLetter(password);
+        boolean actual = PasswortChecker.pwLetter(password);
         //then
+        assertEquals(expected, actual);
+
+    }
+
+    @Test
+    void shouldReturnValidStringIfPasswordIsValid() {
+        //given
+        String password = "FGjhgf12457000";
+        String expected = "Your password is valid";
+        //when
+        String actual = PasswortChecker.checkPassword(password);
+        //then
+        assertEquals(expected, actual);
+
+    }
+
+    @Test
+    void shouldReturnReminderForUpperLowerCases() {
+        //given
+        String password = "1234JKLHHZ";
+        String expected = "Your password should contain lower AND uppercase letters";
+        String actual = PasswortChecker.checkPassword(password);
         assertEquals(expected, actual);
     }
 
+    @Test
+    void shouldReturnReminderForNumbers() {
+        String password = "sdfghjkkiuzjh";
+        String expected = "Your password should contain numbers";
+        String actual = PasswortChecker.checkPassword(password);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void shouldReturnReminderIfPasswordLessThan8Char() {
+        String password = "12jk";
+        String expected = "Your Password should be at least 8 characters long";
+        String actual = PasswortChecker.checkPassword(password);
+        assertEquals(expected, actual);
+    }
 }
